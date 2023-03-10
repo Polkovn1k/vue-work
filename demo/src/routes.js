996057333417c55dce1e6e4c527534a211e39e1e
Polkovn1k <wa0s@narod.ru>
2023-03-10 23:07:12 +0300

    upd

--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,5 +1,9 @@
 export default [{
-    name: 'Counter Base',
+    name: 'Counter Base 1',
     path: '/counter-0',
     component: () => import('./demo0/DemoCounter.vue'),
+},{
+    name: 'Counter Base 2',
+    path: '/counter-1',
+    component: () => import('./demo1/DemoCounter.vue'),
 }]
